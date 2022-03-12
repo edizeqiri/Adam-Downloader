003f275875612a763f2eac55c7626ec94369e17d
edizeqiri <edi54955@gmail.com>
2022-03-12 20:57:36 +0100

    add qol updates

--- LoadnZip.py
+++ LoadnZip.py
@@ -2,9 +2,24 @@ from webbot import Browser
 import os
 import zipfile
 import time
+import bs4
+import lxml.html
+from waiting import wait
 
-def run(email,passwort,path,url,Vorlesungen):
+def downloads_finished(path):
+    for file in os.listdir(path):
+        if file.endswith(".zip"):
+            continue
+        else:
+            return False
+    return True
 
+def run(email,passwort,path,url,amount):
+    
+    # make dir in path
+    path = os.path.join(path, "Vorlesungen")
+    os.mkdir(path)
+    
     # open Browser to url
     web = Browser(showWindow=True, downloadPath=path)
     web.maximize_window()
@@ -17,7 +32,15 @@ def run(email,passwort,path,url,Vorlesungen):
     web.type(email)
     web.type(passwort , into='Password')
     web.click('Anmelden')
-    web.click("Herbstsemester")
+
+    # scrape html source code to get lectures
+    source = web.get_page_source();
+    soup = bs4.BeautifulSoup(source, "html.parser")
+    div = soup.find_all("h4", {"class": "il_ContainerItemTitle"})
+    content = str(div)
+    t = lxml.html.fromstring(content)
+    Vorlesungen = t.text_content()[1:].split(", ")
+    Vorlesungen = Vorlesungen[:amount]
 
     # press download buttons
     preString = "bl_cb_"
@@ -35,10 +58,12 @@ def run(email,passwort,path,url,Vorlesungen):
         web.click('Herunterladen')
         web.go_back()
         web.go_back()
-        web.click("Herbstsemester")
-    time.sleep(30) #TODO: add wait till finished
 
+    # wait till all downloads finishfinished
+    wait(lambda: downloads_finished(path), timeout_seconds=180, waiting_for="something to be ready")
+   
 
+   
     # unzip to folder
     files=os.listdir(path)
     for file in files:
@@ -48,11 +73,11 @@ def run(email,passwort,path,url,Vorlesungen):
             filedir = path+'/'+file[0:len(file) - 3]
             os.makedirs(filedir)
             for names in zip_file.namelist():
-                zip_file.extract(names,filedir) #TODO: Extract only needed files
+                zip_file.extract(names,filedir)
             zip_file.close()
 
     #deletes zip files
     for file in files:
         if file.endswith('.zip'):
             filePath=path+'/'+file
-            os.remove(filePath) 
+            os.remove(filePath)
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import LoadnZip
 import codecs
 import os
 
+
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 #pathData = 'C:\\Users\\edi54\\Documents\\Programmierprojekt\\Python and Shit\\Data.txt'
 pathDownload = 'C:/Users/Public/Documents'
-
 try:
     # open file and save to lines
     with codecs.open(os.path.join(__location__, 'Data.txt'),'r','utf-8') as file:
@@ -22,7 +22,7 @@ except FileNotFoundError:
     with codecs.open(os.path.join(__location__, 'Data.txt'),'r','utf-8') as file:
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
-if len(lines) >= 5:
+if len(lines) >= 4:
 
     # save data
     email = lines[0]
@@ -32,9 +32,7 @@ if len(lines) >= 5:
     Vorlesungen = [None] * (len(lines) - 10)
     for i in range(4,len(lines)):
         Vorlesungen.append(lines[i])
-    LoadnZip.run(email,passwort,pathDownload,url,Vorlesungen)
+    LoadnZip.run(email,passwort,pathDownload,url,8)
 else:
     #TODO: make them repeat above
     print("penis")
-
-
